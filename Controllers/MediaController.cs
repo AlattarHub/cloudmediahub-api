ca391115e20e440202970018117715c8cdea2dcd
Mostafa Alattar <Mustafa.Alattar@hotmail.com>
2026-02-28 02:34:54 +0200

    Clean pipline trigger

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -29,7 +29,7 @@ namespace CloudMediaHub.Api.Controllers
         public IActionResult Test()
         {
             var container = _blobService.GetContainer();
-            return Ok(container.AccountName + ": NEW");
+            return Ok(container.AccountName + ": NEW RESPONSE");
         }
 
         [HttpPost("upload")]
